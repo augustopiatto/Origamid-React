a95e62d3bbf7d79b2430a13a14f471901e6d49cf
augustopiatto <ampiatto@hotmail.com>
2023-06-27 20:30:01 -0300

    ate aula 7 parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
+const titulo = <h1>Esse é um título</h1>;
+
 const App = () => {
-  return <div>App React</div>;
+  const random = Math.random();
+  const ativo = false;
+
+  function mostrarNome(sobrenome) {
+    return 'Augusto ' + sobrenome;
+  }
+
+  const carro = {
+    marca: 'Ford',
+    rodas: '4',
+  };
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: '2rem',
+  };
+
+  return (
+    <>
+      {titulo}
+      <p>
+        {true ? 'adas' : 'ewrtui'} - {10}
+        {mostrarNome('Rafael')}
+      </p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      {/* Objeto inteiro não é aceito */}
+      {/* <p>{carro}</p> */}
+      <p className={ativo ? 'ativo' : 'inativo'}>{(random * 1000) / 50}</p>
+    </>
+  );
 };
 
 export default App;
