fe2e2b4cd307782654f489869ac32245e9c9b046
Augusto <ampiatto@hotmail.com>
2023-07-06 10:36:02 -0300

    Base do projeto final

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import React from 'react';
-import Produtos from './pages/Produtos';
-import Produto from './pages/Produto';
-import Contato from './pages/Contato';
-import './App.css';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import React from "react";
 
 const App = () => {
-  // Utilize a API abaixo para puxar a lista de produto
-  // https://ranekapi.origamid.dev/json/api/produto
-  // Cada produto possui o id, o mesmo pode ser passado na api para retornar os dados desse produto específico
-  // https://ranekapi.origamid.dev/json/api/produto/notebook
-  return (
-    <BrowserRouter>
-      <div className="body">
-        <div className="buttons-container">
-          <NavLink to="/" end className="button">
-            Produtos
-          </NavLink>
-          <NavLink to="/contato" className="button">
-            Contato
-          </NavLink>
-        </div>
-        <Routes>
-          <Route path="/" element={<Produtos />} />
-          <Route path="produto/:id" element={<Produto />} />
-          <Route path="/contato" element={<Contato />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  return <div>App React</div>;
 };
 
 export default App;
