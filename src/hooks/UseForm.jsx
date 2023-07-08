8c80b08acd34212f914a89d9a810add676a46d4f
Augusto <ampiatto@hotmail.com>
2023-07-08 13:57:41 -0300

    Login Criar

--- src/hooks/UseForm.jsx
+++ src/hooks/UseForm.jsx
@@ -5,6 +5,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um e-mail válido",
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      "A senha precisa ter 1 character maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo 8 characteres.",
+  },
 };
 const useForm = (type) => {
   const [value, setValue] = React.useState("");
--- src/pages/login/LoginCreate.jsx
+++ src/pages/login/LoginCreate.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import useForm from "../../hooks/UseForm";
+import Input from "../../components/html_components/Input";
+import Button from "../../components/html_components/Button";
+import { USER_POST } from "../../api";
+import { UserContext } from "../../contexts/UserContext";
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>;
+  const username = useForm();
+  const email = useForm("email");
+  const password = useForm("password");
+
+  const { userLogin } = React.useContext(UserContext);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+    const json = await response.json();
+  };
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
