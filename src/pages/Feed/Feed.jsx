a1de10413186bf8c53397054b2132765825f1d99
Augusto <ampiatto@hotmail.com>
2023-07-10 06:14:13 -0300

    Feed Modal 1

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -3,10 +3,12 @@ import FeedModal from "./FeedModal.jsx";
 import FeedPhotos from "./FeedPhotos.jsx";
 
 const Feed = () => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+
   return (
     <div>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && <FeedModal photo={modalPhoto} />}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </div>
   );
 };
--- src/pages/Feed/FeedModal.jsx
+++ src/pages/Feed/FeedModal.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./FeedModal.module.css";
+import useFetch from "../../hooks/useFetch";
+import { PHOTO_GET } from "../../api";
+import Error from "../../helpers/Error.jsx";
+import Loading from "../../helpers/Loading.jsx";
+import PhotoContent from "../../components/Photo/PhotoContent.jsx";
 
-const FeedModal = () => {
-  return <div></div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    const { response, json } = request(url, options);
+  }, [photo, request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data) return <PhotoContent data={data} />;
+  else return null;
 };
 
 export default FeedModal;
