c386f8c42ef5b8ab27406c3f28ef4e4dc0f1f71d
Augusto <ampiatto@hotmail.com>
2023-07-12 00:51:29 -0300

    Biblioteca Externa 1

--- src/components/user/UserStats.jsx
+++ src/components/user/UserStats.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Head from "../../helpers/Head";
+import useFetch from "../../hooks/useFetch.jsx";
+import { STATS_GET } from "../../api";
+import Loading from "../../helpers/Loading.jsx";
+import Error from "../../helpers/Error.jsx";
+import UserStatsGraph from "./UserStatsGraphs";
 
-const UserStatus = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      UserStatus
-    </div>
-  );
+const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    };
+    getData();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (!loading && data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraph data={data} />
+      </div>
+    );
+  else return null;
 };
 
-export default UserStatus;
+export default UserStats;
